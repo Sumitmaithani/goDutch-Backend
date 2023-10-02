f7d353890987e9a1f8fb1e8ea8f9a525b01b4d18
Sumitmaithani <sumitmaithani824@gmail.com>
2023-10-02 13:51:21 +0530

    added image feature

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
+  image: { type: String, default: "" },
 });
 
 const User = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -40,9 +40,14 @@ app.use(cors());
 //POST REGISTER
 app.post("/register", async (req, res) => {
   try {
-    const { email, username, password } = req.body;
+    const { email, username, password, image } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ email, username, password: hashedPassword });
+    const newUser = new User({
+      email,
+      username,
+      password: hashedPassword,
+      image,
+    });
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
